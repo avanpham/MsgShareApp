d73830d25b9697a5d622452ff61dcb53819e72c2
alanpham <avanpham@yahoo.com>
2019-09-30 14:26:06 -0700

    Section Two, Video three

---
+++ app/src/main/java/com/alanvp/msgshareapp/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.alanvp.msgshareapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnShowToast.setOnClickListener {
+            // code
+            Log.i("MainActivity", "Button was clicked !")
+
+            Toast.makeText(this, "Button was clicked !", Toast.LENGTH_SHORT).show()
+        }
+
+        btnSendingMsgToNextActivity.setOnClickListener{
+            Toast.makeText(this, "Second Button was clicked!", Toast.LENGTH_SHORT).show()
+
+            val message: String = etUserMessage.text.toString()
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
+
+        }
+    }
+}
---
+++ app/src/main/java/com/alanvp/msgshareapp/SecondActivity.kt
@@ -0,0 +1,16 @@
+package com.alanvp.msgshareapp
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SecondActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+
+    }
+
+
+}
