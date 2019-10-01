1090601657a4ed97b009720c601f257cb7a8dae9
alanpham <avanpham@yahoo.com>
2019-09-30 19:46:19 -0700

    Section Two, Video three

--- app/src/main/java/com/alanvp/msgshareapp/MainActivity.kt
+++ app/src/main/java/com/alanvp/msgshareapp/MainActivity.kt
@@ -23,9 +23,10 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(this, "Second Button was clicked!", Toast.LENGTH_SHORT).show()
 
             val message: String = etUserMessage.text.toString()
-            Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
-
             val intent = Intent(this, SecondActivity::class.java)
+
+            intent.putExtra("user_message", message)
+
             startActivity(intent)
 
         }
